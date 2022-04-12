9d01c616f1686130e48a125ff35309bba2364f2d
timrusin <timrusin@gmail.com>
2022-04-12 15:23:05 -0500

    nav bad getting interesting

--- src/cpmponents/Navbar.js
+++ src/cpmponents/Navbar.js
@@ -4,14 +4,20 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
+      <>
+      <div className="title">
+        <h1>Tim Rusin</h1>
+        <h4>software engineer</h4>
+      </div>
     <div className="navbar">
-      <nav className='nav-links'>
-            <Link to="/projects">Projects</Link>
-            <Link to="/">Home</Link>
-            <Link to="/">About</Link>
-            <Link to="/">Contact</Link>
+      <nav className="nav-links">
+        <Link to="/projects">Projects</Link>
+        <Link to="/">Home</Link>
+        <Link to="/">About</Link>
+        <Link to="/">Contact</Link>
       </nav>
     </div>
+    </>
   );
 }
 
