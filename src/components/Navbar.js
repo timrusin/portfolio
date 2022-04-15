5997afe6881ddf0bbff1361c78206d5c53aa5ac5
timrusin <timrusin@gmail.com>
2022-04-15 00:36:47 -0500

    drop down mobile menu working issues with link postioning not being static though

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,12 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   const [click, setClick] = useState(false);
   const handleClick = () => setClick (!click)
+  const closeMenu = () => setClick (false)
 
   return (
     <div className="container sticky">
       <div id="title">
-        <Link to="/">
+        <Link to="/" onClick={closeMenu}>
           <h1>Tim Rusin</h1>
         </Link>
         <h4>software engineer</h4>
@@ -18,21 +19,22 @@ const Navbar = () => {
         <div className="menu-icon" onClick={handleClick}>
           <i className={click ? "fas fa-times" : "fas fa-bars"} />
         </div>
-
-        <nav className="nav-links">
-          <Link to="/" id="home">
-            Home
-          </Link>
-          <Link to="/about" id="about">
-            About
-          </Link>
-          <Link to="/projects" id="projects">
-            Projects
-          </Link>
-          <Link to="/contact" id="contact">
-            Contact
-          </Link>
-        </nav>
+        <div className={click ? "nav-menu active" : "nav-menu"}>
+          <nav className="nav-links">
+            <Link className="nav-item" to="/" onClick={closeMenu}>
+              Home
+            </Link>
+            <Link className="nav-item" to="/about" onClick={closeMenu}>
+              About
+            </Link>
+            <Link className="nav-item" to="/projects" onClick={closeMenu}>
+              Projects
+            </Link>
+            <Link className="nav-item" to="/contact" onClick={closeMenu}>
+              Contact
+            </Link>
+          </nav>
+        </div>
       </div>
     </div>
   );
