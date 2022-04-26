8f1f478f7891a38e9f97121b7915b453e0a2af17
timrusin <timrusin@gmail.com>
2022-04-26 11:49:19 -0500

    created react strap modals for proj cards not crazy about them yet

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,13 +1,33 @@
+import { useState } from 'react';
 import './ProjectCard.css'
+import { ModalFooter, Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
 
 const ProjectCard = (props) => {
+const [modal, setModal] = useState (false)
 
   return (
-    <div className="p-card-bg">
+    <div className="p-card-bg" onClick={() => setModal(!modal)}>
       <div className="p-card-title">{props.title}</div>
-      {/* <div className="p-card-description">{props.description}</div> */}
       <div className="p-card-stack">Tech Stack: {props.stack}</div>
       <img className="p-card-image" src={props.image} alt={props.alt} />
+      <Modal id="modal" isOpen={modal}>
+        <ModalHeader toggle={()=>setModal(!modal)}>{props.title}</ModalHeader>
+        <ModalBody>
+          <div className="p-card-description">{props.description}</div>
+        </ModalBody>
+        <ModalFooter>
+          <a href = {props.git} target="new">
+          <Button color="primary" onClick={()=>setModal(!modal)}>
+            Source Code
+          </Button>{" "}
+          </a>
+          <a href = {props.link} target="new">
+          <Button color="primary" onClick={()=>setModal(!modal)}>
+            deployed link
+          </Button>{" "}
+          </a>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 }
--- src/components/ProjectModal.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ProjectModal = () => {
-  return (
-    <div>ProjectModal</div>
-  )
-}
-
-export default ProjectModal
