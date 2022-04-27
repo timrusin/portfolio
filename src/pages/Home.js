b955d2511e2713fe061722e4d2678274ca09bc3d
timrusin <timrusin@gmail.com>
2022-04-26 22:44:07 -0500

    playing around with video on hero page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Home.css'
+import homeVideo from '../images/IMG_5417.mp4'
 
 const Home = () => {
   return (
     <div className='home-page-container'>
-      <h1>still deciding what to do here</h1>
+      <video className="home-video" autoPlay loop muted>
+        <source src={homeVideo} type='video/mp4'/>
+      </video>
     </div>
   )
 }
