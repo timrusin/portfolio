946f19c7003333d20055379ea2abab18db9dab96
timrusin <timrusin@gmail.com>
2022-04-26 23:32:29 -0500

    removed video that wasnt working out need to come up with clever hero page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,7 @@ import homeVideo from '../images/IMG_5417.mp4'
 const Home = () => {
   return (
     <div className='home-page-container'>
-      <video autoplay preload="auto" loop muted playsinline id="home-video">
-        <source src={homeVideo} type='video/mp4'/>
-      </video>
+     <h1>back to the drawing board</h1>
     </div>
   )
 }
