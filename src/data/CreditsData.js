6570b9f83a58990ffa3dae94c962223ebde02310
timrusin <timrusin@gmail.com>
2022-04-30 15:07:59 -0500

    created rough start for CreditsData

---
+++ src/data/CreditsData.js
@@ -0,0 +1,13 @@
+export const CreditsData = [
+
+    {
+        artist: "Hide and Seek",
+        album: "Hide and Seek is a web based game built in vanilla HTML, Javascript, and CSS. The game was originally inspired by Milton Bradley's classic BattleShip board game but then took on a life of its own as I drew artistic vision from the 'Stranger Things' franchise. This was my first ever dev project while attending a Software Engineering bootcamp with General Assembly.",
+        release: "",
+        image: "https://github.com/timrusin/portfolio/blob/main/src/images/project_img/hideandseek.png?raw=true",
+        alt: "hide and seek game",
+        spotify: "HTML, JavaScript, CSS",
+    },
+]
+
+export default CreditsData
