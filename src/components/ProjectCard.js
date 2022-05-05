366d16ae9928b0e2adcf018e700878303c67a29b
timrusin <timrusin@gmail.com>
2022-05-05 14:46:47 -0500

    re assigned a new modal button hidden class to solve close button not hidding

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -10,10 +10,10 @@ const openModal = () => setModal(true)
   return (
     <div className="p-card-bg">
       <div className="p-card-title">{props.title}</div>
-      <div className={modal ? 'modal-button' : 'modal-hidden'} onClick={closeModal}>CLOSE</div>
+      <div className={modal ? 'modal-button' : 'modal-button-hidden'} onClick={closeModal}>CLOSE</div>
       <div className={modal ? "modal-showing" : "modal-hidden"}><Modal {...props} modal={modal}/></div>
       <div className="p-card-stack">Tech Stack: {props.stack}</div>
-      <div className={!modal ? 'modal-button' : 'modal-hidden'} onClick={openModal}>MORE INFO</div>
+      <div className={!modal ? 'modal-button' : 'modal-button-hidden'} onClick={openModal}>MORE INFO</div>
       <img className="p-card-image" src={props.image} alt={props.alt} />
     </div>
   );
