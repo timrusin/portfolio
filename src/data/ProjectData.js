60b99c1416aef4f9c3e348d8432d30f9e115ccab
timrusin <timrusin@gmail.com>
2022-05-06 22:05:57 -0500

    saved project images to rusin audio google drive to have smaller images, couldnt figure out how to do this with data without having a url

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,9 +1,9 @@
-export const ProjectData = [
+const ProjectData = [
 
     {
         title: "Hide and Seek",
         description: "Hide and Seek is a web based game built in vanilla HTML, Javascript, and CSS. The game was originally inspired by Milton Bradley's classic BattleShip board game but then took on a life of its own as I drew artistic vision from the 'Stranger Things' franchise. This was my first ever dev project while attending a Software Engineering bootcamp with General Assembly.",
-        image: "https://github.com/timrusin/portfolio/blob/main/src/images/project_img/hideandseek.png?raw=true",
+        image: 'https://drive.google.com/uc?id=1QYEBwXx6DGprA4cuO2tfZHhdyJQ_DjpP',
         alt: "hide and seek game",
         stack: "HTML, JavaScript, CSS",
         git: "https://github.com/timrusin/HideAndSeek",
@@ -13,7 +13,7 @@ export const ProjectData = [
     {
         title: "Star Wars for Dummies",
         description: "Star Wars for Dummies was my first single page application built with React.js. It is also the first time I utilized a public API to fetch data. The app was a good introduction to some basic props and state management in React.js, learning about APIs, and continued practice with CSS to fit the Star Wars aesthetic as best I could.",
-        image: "https://github.com/timrusin/portfolio/blob/main/src/images/project_img/starwars.png?raw=true",
+        image: 'https://drive.google.com/uc?id=1YapFEIMsfdlond0m836upOEAtq1Xr_15',
         alt: "Star Wars for Dummies app",
         stack: "React.js, Node.js, API",
         git: "https://github.com/timrusin/StarWarsForDummies",
@@ -23,7 +23,7 @@ export const ProjectData = [
     {
         title: "Air Pods",
         description: "Air Pods was my first team project. We were assigned the name 'Team Air' and wanted to make a podcast search app, so we ran with the whole 'Air Pods' thing. There was a lot of struggle with accessing with we wanted from Sporify's public API, as well as trying to get user auth to work as planned. I really enjoyed working with my other team members on this project and although it was rough getting through it in the time allotted, we were overall pretty proud of what we accomplished together. FEEL FREE TO LOG IN WITH username:'user' and password:'password' to try it out.",
-        image: "https://github.com/timrusin/portfolio/blob/main/src/images/project_img/airpods.png?raw=true",
+        image: "https://drive.google.com/uc?id=1p2zFffbczNK-mRIxmHMg9xj8bZzRoFLC",
         alt: "Podcast app",
         stack: "MERN",
         git: "https://github.com/timrusin/airpods_frontend" ,
@@ -34,7 +34,7 @@ export const ProjectData = [
     {
         title: "Selena's Bus",
         description: "Selena's Bus is a children's learning game that I created for my daughter, and was my capstone project while attending General Assembly. We had to implement a technology into the project that we didn't learn in class together, so I decided to build a mobile app with React Native. I also felt I struggled a bit with React.js in general on previous projects so I took this on to push myself even harder. I learned a lot from it. The app is not currently deployed anywhere, but feel free to check out the source code and even clone it down and run it if you have the ability to do so.",
-        image: "https://github.com/timrusin/portfolio/blob/main/src/images/project_img/selenabus.png?raw=true",
+        image: "https://drive.google.com/uc?id=1kbxzcofcQQKLockQfWRYtFwH_WGCAQ8B",
         alt: "Children Learning Game",
         stack: "MERN (React-Native)",
         git: "https://github.com/timrusin/Selena-Bus",
@@ -44,7 +44,7 @@ export const ProjectData = [
     {
         title: "Cstar311 Photography",
         description: "This is my first project that I have started on (aside from the website you're on right now) once I graduated from school. This will be a landing site for my father in law's professional photography. This is when I really started feeling more comfortable and confident in React.js.",
-        image: "https://github.com/timrusin/portfolio/blob/main/src/images/project_img/cstar.png?raw=true",
+        image: "https://drive.google.com/uc?id=1iNaJEVr9Dh5vKWVfFgGgCajrvOnmy0ED",
         alt: "Cstar311 Photography site",
         stack: "React.js, Node.js",
         git: "https://github.com/timrusin/cstar311" ,
