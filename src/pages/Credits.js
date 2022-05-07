469f36d790a8513db6d14c4ff866e20b2bffa880
timrusin <timrusin@gmail.com>
2022-05-06 23:10:33 -0500

    built JBTV data

--- src/pages/Credits.js
+++ src/pages/Credits.js
@@ -7,13 +7,18 @@ import AlbumCredits from '../data/AlbumCreditsData'
 const Credits = () => {
   return (
     <>
-    <div className='album-credits'>Album Credits</div>
+      <div className="album-credits">Album Credits</div>
       <div className="credits-page-container">
-        {AlbumCredits.map(item => {
+        {AlbumCredits.map((item) => {
           return <AlbumCredCard key={item.id} {...item} />;
         })}
       </div>
-      <div className='album-credits'>JBTV Credits</div>
+      <div className="album-credits">JBTV Credits</div>
+      <div className="album-credits jbtv">
+        I worked with all of the following artists at JBTV on a personal level as a
+        recording, mixing, and mastering engineer for their live to tape TV
+        performances.
+      </div>
     </>
   );
 }
