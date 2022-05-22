b8ed379f7358571f19bb0c744fcac8ba8dcaa6fe
timrusin <timrusin@gmail.com>
2022-05-22 10:39:11 -0500

    contact page widgets

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,37 @@
 import React from 'react'
 import './Contact.css'
+import { FaGithub } from 'react-icons/fa'
+import { FaLinkedin } from 'react-icons/fa'
+import { FaSoundcloud } from 'react-icons/fa'
+import { FaFacebook } from 'react-icons/fa'
 
 const Contact = () => {
   return (
     <div className="contact-page-container">
         <h1>Contact page is on it's way!</h1>
-        <p>you can reach me at timrusin@gmail.com</p>
+        <p>for now, you can reach me at timrusin@gmail.com</p>
+        <div className="about-icons-container">
+          <a href="https://github.com/timrusin" target="new">
+            <div className="about-logos">
+              <FaGithub size={25} />
+            </div>
+          </a>
+          <a href="https://soundcloud.com/tjblender" target="new">
+            <div className="about-logos">
+              <FaSoundcloud size={25} />
+            </div>
+          </a>
+          <a href="https://www.linkedin.com/in/timrusin/" target="new">
+            <div className="about-logos">
+              <FaLinkedin size={25} />
+            </div>
+          </a>
+          <a href="https://www.facebook.com/rusinaudio" target="new">
+            <div className="about-logos">
+              <FaFacebook size={25} />
+            </div>
+          </a>
+        </div>
     </div>
   )
 }
