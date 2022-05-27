03cea24ce916c7e6ef437ae10ef9dd6c8a78efc8
timrusin <timrusin@gmail.com>
2022-05-26 23:52:24 -0500

    fixed for redirect check format on phone

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,7 +1,7 @@
 import {useState} from 'react'
 import "./ContactForm.css"
 
-const FORM_ENDPOINT = "https://submit-form.com/48S3pn6x"
+const FORM_ENDPOINT = "https://public.herotofu.com/v1/ef4a2090-dd44-11ec-8be6-4f814e368566"
 
 const ContactForm = () => {
     const [submitted, setSubmitted] = useState(false)
@@ -12,55 +12,47 @@ const ContactForm = () => {
     }
     if (submitted){
         return (
-            <>
-                <div className='thank-you'>Thank you!</div>
-                <div className='talk-soon'>Talk to you soon</div>
-            </>
+            <div className='submitted-message' >
+                <h1 className='thank-you'>Thank you!</h1>
+                <h2 className='talk-soon'>Talk soon</h2>
+            </div>
         )
     }
     return (
-        <form
-          action={FORM_ENDPOINT}
-          onSubmit={handleSubmit}
-          method="POST"
-          target="_blank"
-        >
-          <div className="input-field">
-            <input
-              type="text"
-              placeholder="And you are...?"
-              name="name"
-              className="input name"
-              required
-            />
-          </div>
-          <div className="input-field">
-            <input
-              type="email"
-              placeholder="Your Email"
-              name="email"
-              className="input email"
-              required
-            />
-          </div>
-          <div className="input-field">
-            <textarea
-              placeholder="What's up?"
-              name="message"
-              className="input message"
-              required
-            />
-          </div>
-          <div className="input-field">
-            <button
-              className="contact-button"
-              type="submit"
-            >
-              - Send -
-            </button>
-          </div>
-        </form>
-      );
+      <form className='contact-form' action={FORM_ENDPOINT} onSubmit={handleSubmit} method="POST">
+        <div className="input-field">
+          <input
+            type="text"
+            placeholder="And you are...?"
+            name="name"
+            className="input name"
+            required
+          />
+        </div>
+        <div className="input-field">
+          <input
+            type="email"
+            placeholder="Your Email"
+            name="email"
+            className="input email"
+            required
+          />
+        </div>
+        <div className="input-field">
+          <textarea
+            placeholder="What's up?"
+            name="message"
+            className="input message"
+            required
+          />
+        </div>
+        <div className="input-field">
+          <button className="contact-button" type="submit">
+            - Send -
+          </button>
+        </div>
+      </form>
+    );
     };
 
 export default ContactForm
