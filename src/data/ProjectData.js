209ba7e2151c3bcc061f57661c890c6af329d396
timrusin <timrusin@gmail.com>
2022-06-06 22:03:35 -0500

    shortened some of the project descriptions

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -2,7 +2,7 @@ const ProjectData = [
 
     {
         title: "Hide and Seek",
-        description: "Hide and Seek is a web based game built in vanilla HTML, Javascript, and CSS. The game was originally inspired by Milton Bradley's classic BattleShip board game but then took on a life of its own as I drew artistic vision from the 'Stranger Things' franchise. This was my first ever dev project while attending a Software Engineering bootcamp with General Assembly.",
+        description: "Hide and Seek is a web based game built in vanilla HTML, Javascript, and CSS. Originally inspired by Milton Bradley's classic BattleShip board game that then took on a life of its own as I drew artistic vision from the 'Stranger Things' franchise. This was my first ever dev project while attending a Software Engineering bootcamp with General Assembly.",
         image: 'https://drive.google.com/uc?id=1QYEBwXx6DGprA4cuO2tfZHhdyJQ_DjpP',
         alt: "hide and seek game",
         stack: "HTML, JavaScript, CSS",
@@ -22,7 +22,7 @@ const ProjectData = [
 
     {
         title: "Air Pods",
-        description: "Air Pods was my first team project. We were assigned the name 'Team Air' and wanted to make a podcast search app, so we ran with the whole 'Air Pods' thing. There was a lot of struggle with accessing what we wanted from Spotify's public API, as well as trying to get user auth to work as planned. I really enjoyed working with my other team members on this project and although it was rough getting through it in the time allotted, we were overall pretty proud of what we accomplished together. Feel free to log in using: username:'user' and password:'password' to try it out.",
+        description: "Air Pods was my first team project.  There was a lot of struggle with accessing what we wanted from Spotify's public API, as well as trying to get user auth to work as planned. I really enjoyed working with my other team members on this project and although it was rough getting through it in the time allotted. Feel free to log in using: username:'user' and password:'password' to try it out.",
         image: "https://drive.google.com/uc?id=1p2zFffbczNK-mRIxmHMg9xj8bZzRoFLC",
         alt: "Podcast app",
         stack: "MERN",
@@ -33,7 +33,7 @@ const ProjectData = [
     
     {
         title: "Selena's Bus",
-        description: "Selena's Bus is a children's learning game that I created for my daughter, and was my capstone project while attending General Assembly. We had to implement a technology into the project that we didn't learn in class together, so I decided to build a mobile app with React Native. I also felt I struggled a bit with React.js in general on previous projects so I took this on to push myself even harder. I learned a lot from it. The app is not currently deployed anywhere, but feel free to check out the source code and even clone it down and run it if you have the ability to do so.",
+        description: "Selena's Bus is a children's learning game that I created for my daughter. This was my capstone project while attending General Assembly. We had to implement a technology into the project that we didn't learn in class together, so I decided to build a mobile app with React Native. The app is not currently deployed anywhere, but feel free to check out the source code and even clone it down and run it if you have the ability to do so.",
         image: "https://drive.google.com/uc?id=1kbxzcofcQQKLockQfWRYtFwH_WGCAQ8B",
         alt: "Children Learning Game",
         stack: "MERN (React-Native)",
@@ -43,7 +43,7 @@ const ProjectData = [
     
     {
         title: "Cstar311 Photography",
-        description: "This is my first project that I have started on (aside from the website you're on right now) once I graduated from school. This will be a landing site for my father in law's professional photography. This is when I really started feeling more comfortable and confident in React.js.",
+        description: "This is my first project that I have started on (aside from the website you're on right now) once I graduated from school. This will be a landing site for my father in law's professional photography.  This is when I really started feeling more comfortable and confident in React.js.",
         image: "https://drive.google.com/uc?id=1iNaJEVr9Dh5vKWVfFgGgCajrvOnmy0ED",
         alt: "Cstar311 Photography site",
         stack: "React.js, Node.js",
