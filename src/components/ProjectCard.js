7076fe80b72825aad251839556f979c68701b51c
timrusin <timrusin@gmail.com>
2022-06-06 20:55:28 -0500

    new project cards weird stack size issue on click still with mobile

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,17 +4,26 @@ import './ProjectCard.css'
 
 const ProjectCard = (props) => {
 const [modal, setModal] = useState (false)
-const closeModal = () => setModal(false)
-const openModal = () => setModal(true)
+const [active, setActive] = useState (false)
+const focus = () => {
+  setActive(true) 
+  setModal(true)
+}
+const unFocus = () => {
+  setActive(false) 
+  setModal(false)
+}
+// const closeModal = () => setModal(false)
+// const openModal = () => setModal(true)
 
   return (
-    <div onMouseLeave={closeModal} className="p-card-bg">
-      <div className="p-card-title">{props.title}</div>
-      <div className={modal ? 'modal-button' : 'modal-button-hidden'} onClick={closeModal}>CLOSE</div>
+    <div onMouseEnter ={focus} onMouseLeave={unFocus} className="p-card-bg">
+      <div className={active ? "p-card-title" : "title-hidden"}>{props.title}</div>
+      {/* <div className={modal ? 'modal-button' : 'modal-button-hidden'} onClick={closeModal}>CLOSE</div> */}
       <div className={modal ? "modal-showing" : "modal-hidden"}><Modal {...props} modal={modal}/></div>
-      <div className="p-card-stack">Tech Stack: {props.stack}</div>
-      <div className={!modal ? 'modal-button' : 'modal-button-hidden'} onClick={openModal}>MORE INFO</div>
-      <img className="p-card-image" src={props.image} alt={props.alt} />
+      <div className= {active ? "p-card-stack" : "stack-hidden"}>Tech Stack: {props.stack}</div>
+      {/* <div className={!modal ? 'modal-button' : 'modal-button-hidden'} onClick={openModal}>MORE INFO</div> */}
+      <img className={!active? "p-card-image" : "smaller"} src={props.image} alt={props.alt} />
     </div>
   );
 }
