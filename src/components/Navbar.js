d7632a86a39bb6212f990392be8023ed02fba964
timrusin <timrusin@gmail.com>
2022-06-06 21:03:45 -0500

    adjusted project nav offset a bit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -61,7 +61,7 @@ const Navbar = () => {
               to="projects"
               spy={true}
               smooth={true}
-              offset={-60}
+              offset={-90}
               duration={500}
               onClick={closeMenu}
             >
