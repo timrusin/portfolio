daf194131513bb4cd4a6eec8c469c87ea798a2e9
timrusin <timrusin@gmail.com>
2022-06-09 06:10:46 -0500

    bumped proj and cred offset back to -65

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -61,7 +61,7 @@ const Navbar = () => {
               to="projects"
               spy={true}
               smooth={true}
-              offset={-70}
+              offset={-65}
               duration={500}
               onClick={closeMenu}
             >
@@ -74,7 +74,7 @@ const Navbar = () => {
               to="credits"
               spy={true}
               smooth={true}
-              offset={-70}
+              offset={-65}
               duration={500}
               onClick={closeMenu}
             >
