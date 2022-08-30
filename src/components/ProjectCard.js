f1e43a9489c280f69ef683397ad24e42228ab1a9
Timothy Rusin <94573769+timrusin@users.noreply.github.com>
2022-08-30 13:17:40 -0500

    removed unused code
    Removed unused modal button code

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,6 +5,7 @@ import './ProjectCard.css'
 const ProjectCard = (props) => {
 const [modal, setModal] = useState (false)
 const [active, setActive] = useState (false)
+
 const focus = () => {
   setActive(true) 
   setModal(true)
@@ -13,16 +14,12 @@ const unFocus = () => {
   setActive(false) 
   setModal(false)
 }
-// const closeModal = () => setModal(false)
-// const openModal = () => setModal(true)
 
   return (
     <div onClick={focus} onMouseLeave={unFocus} className= {active ? "p-card-bg bigger" : "p-card-bg"}>
       <div className={active ? "p-card-title" : "title-hidden"}>{props.title}</div>
-      {/* <div className={modal ? 'modal-button' : 'modal-button-hidden'} onClick={closeModal}>CLOSE</div> */}
       <div className={modal ? "modal-showing" : "modal-hidden"}><Modal {...props} modal={modal}/></div>
       <div className= {active ? "p-card-stack" : "stack-hidden"}>Tech Stack: {props.stack}</div>
-      {/* <div className={!modal ? 'modal-button' : 'modal-button-hidden'} onClick={openModal}>MORE INFO</div> */}
       <img className={!active? "p-card-image" : "smaller"} src={props.image} alt={props.alt} />
     </div>
   );
