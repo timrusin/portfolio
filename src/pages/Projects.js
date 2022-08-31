9f1a805d303cd8a1ba31c562087c00107730535b
timrusin <timrusin@gmail.com>
2022-08-30 22:25:44 -0500

    added some more button styling to match about section

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,21 +4,20 @@ import Data from '../data/ProjectData'
 import './Project.css'
 
 const Projects = () => {
-  const [selection, setSelection] = useState('')
+  const [selection, setSelection] = useState('website')
 
   const selected = []
   Data.map(item => {
     return item.category === selection && selected.push(item) 
-    
   })
 
   return (
     <>
     <h1 className="projects-title">SOFTWARE PROJECTS</h1>
       <div id="proj-btn-container">
-        <button className="proj-btns" onClick={()=> setSelection('website')}>Web Sites</button>
-        <button className="proj-btns" onClick={()=> setSelection('game')}>Game Dev</button>
-        <button className="proj-btns" onClick={()=> setSelection('application')}>Applications</button>
+        <button className={selection === 'website' ? "proj-btns proj-btns-active" : "proj-btns"} onClick={()=> setSelection('website')}>Web Sites</button>
+        <button className={selection === 'game' ? "proj-btns proj-btns-active" : "proj-btns"} onClick={()=> setSelection('game')}>Game Dev</button>
+        <button className={selection === 'application' ? "proj-btns proj-btns-active" : "proj-btns"} onClick={()=> setSelection('application')}>Applications</button>
       </div>
     <div className="project-page-container">
       {selected.map(item => {
