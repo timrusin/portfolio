a64cedc834d3f9285148ad8a482113b709a3286c
timrusin <timrusin@gmail.com>
2022-09-12 22:01:17 -0500

    setting up categories for album credits want to set up a filter

--- src/pages/Credits.js
+++ src/pages/Credits.js
@@ -7,11 +7,26 @@ import JBTVCredits from '../data/JbtvCreditsData'
 
 
 const Credits = () => {
+
+  const [selection, setSelection] = React.useState("Recoring, Mixing, and Mastering")
+  const selected = []
+
+  AlbumCredits.map(item => {
+    return item.credit === selection && selected.push(item) 
+  })
+
+
   return (
     <>
       <div className="album-credits">Album Credits</div>
+      <div id="proj-btn-container">
+      <button className={selection === 'Recording, Mixing, and Mastering' ? "proj-btns proj-btns-active" : "proj-btns"} onClick={()=> setSelection('Recording, Mixing, and Mastering')}>Recording/Mixing/Mastering</button>
+        <button className={selection === 'Mixing and Mastering' ? "proj-btns proj-btns-active" : "proj-btns"} onClick={()=> setSelection('Mixing and Mastering')}>Mixing/Mastering</button>
+        <button className={selection === 'Mixing' ? "proj-btns proj-btns-active" : "proj-btns"} onClick={()=> setSelection('Mixing')}>Mixing</button>
+        <button className={selection === 'Mastering' ? "proj-btns proj-btns-active" : "proj-btns"} onClick={()=> setSelection('Mastering')}>Mastering</button>
+      </div>
       <div className="credits-page-container">
-        {AlbumCredits.map((item) => {
+        {selected.map((item) => {
           return <AlbumCredCard key={item.id} {...item} />;
         })}
       </div>
