e46196b89e8fc81e66114d3b34205828194b67d5
timrusin <timrusin@gmail.com>
2022-10-10 15:41:56 -0500

    added dev project images to public folder and linked publicly

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -3,7 +3,7 @@ const ProjectData = [
     {
         title: "Hide and Seek",
         description: "Hide and Seek is a web based game built in vanilla HTML, Javascript, and CSS. Originally inspired by Milton Bradley's classic BattleShip board game that then took on a life of its own as I drew artistic vision from the 'Stranger Things' franchise. This was my first ever dev project while attending a Software Engineering bootcamp with General Assembly.",
-        image: 'https://drive.google.com/uc?id=1QYEBwXx6DGprA4cuO2tfZHhdyJQ_DjpP',
+        image: '/assets/images/hideandseek.jpg',
         alt: "hide and seek game",
         stack: "HTML, JavaScript, CSS",
         git: "https://github.com/timrusin/HideAndSeek",
@@ -14,7 +14,7 @@ const ProjectData = [
     {
         title: "Star Wars for Dummies",
         description: "Star Wars for Dummies was my first single page application built with React.js. It is also the first time I utilized a public API to fetch data. The app was a good introduction to some basic props and state management in React.js, learning about APIs, and continued practice with CSS to fit the Star Wars aesthetic as best I could.",
-        image: 'https://drive.google.com/uc?id=1YapFEIMsfdlond0m836upOEAtq1Xr_15',
+        image: '/assets/images/starwars.jpg',
         alt: "Star Wars for Dummies app",
         stack: "React.js, Node.js, API",
         git: "https://github.com/timrusin/starwarsfordummiesrefactored",
@@ -25,7 +25,7 @@ const ProjectData = [
     {
         title: "Air Pods",
         description: "Air Pods was my first team project.  There was a lot of struggle with accessing what we wanted from Spotify's public API, as well as trying to get user auth to work as planned. I really enjoyed working with my other team members on this project and although it was rough getting through it in the time allotted. Feel free to log in using: username:'user' and password:'password' to try it out.",
-        image: "https://drive.google.com/uc?id=1p2zFffbczNK-mRIxmHMg9xj8bZzRoFLC",
+        image: "/assets/images/airpods.jpg",
         alt: "Podcast app",
         stack: "MERN",
         git: "https://github.com/timrusin/airpods_frontend" ,
@@ -37,7 +37,7 @@ const ProjectData = [
     {
         title: "Selena's Bus",
         description: "Selena's Bus is a children's learning game that I created for my daughter. This was my capstone project while attending General Assembly. We had to implement a technology into the project that we didn't learn in class together, so I decided to build a mobile app with React Native. The app is not currently deployed anywhere, but feel free to check out the source code and even clone it down and run it if you have the ability to do so.",
-        image: "https://drive.google.com/uc?id=1kbxzcofcQQKLockQfWRYtFwH_WGCAQ8B",
+        image: "/assets/images/selenabus.jpg",
         alt: "Children Learning Game",
         stack: "MERN (React-Native)",
         git: "https://github.com/timrusin/Selena-Bus",
@@ -47,7 +47,7 @@ const ProjectData = [
     {
         title: "Cstar311 Photography",
         description: "This is my first project that I have started on (aside from the website you're on right now) once I graduated from school. This will be a landing site for my father in law's professional photography.  This is when I really started feeling more comfortable and confident in React.js.",
-        image: "https://drive.google.com/uc?id=1iNaJEVr9Dh5vKWVfFgGgCajrvOnmy0ED",
+        image: "/assets/images/cstar.jpg",
         alt: "Cstar311 Photography site",
         stack: "React.js, Node.js",
         git: "https://github.com/timrusin/cstar311" ,
@@ -57,7 +57,7 @@ const ProjectData = [
     {
         title: "Moto Heaters",
         description: "Motorola Mobility has an amazing roof deck on top of their Chicago office and I was asked to create a QR code and instruction landing page for their new elecrtric cabana heaters.  This was a very simple, yet very fun and effective real-world project that I'm proud to add to my portfolio. This project was designed for, and looks it's best on a mobile device",
-        image: "https://drive.google.com/uc?id=1Aac0LqvHWcvOX__Iwd0peGTotSrO2Xns",
+        image: "/assets/images/MotoHeaters.png",
         alt: "Moto Heaters",
         stack: "HTML, CSS, Quick Response Code",
         git: "https://github.com/timrusin/MotoHeaters" ,
@@ -67,7 +67,7 @@ const ProjectData = [
     {
         title: "Double Down Auto Detailing",
         description: "Auto detailing service website for a long-time old friend of mine. Site is being built as we speak (you read).  I've implemented some really cool CSS tricks to lean into the casino type branding he is looking for.",
-        image: "https://drive.google.com/uc?id=1_Ky1Xb5e8o5DnFt9jPReayHk6m5ue5sH",
+        image: "/assets/images/doubleDown.png",
         alt: "Double Down",
         stack: "React.js, Node.JS",
         git: "https://github.com/timrusin/doubledown" ,
