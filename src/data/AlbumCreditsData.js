c5cb957c2332060d1816e0028761f2ce889f834b
timrusin <timrusin@gmail.com>
2022-12-27 13:21:12 -0600

    fixed helphounds photo

--- src/data/AlbumCreditsData.js
+++ src/data/AlbumCreditsData.js
@@ -2,7 +2,7 @@ export const AlbumCreditsData = [
     
     {
         id: 60,
-        image: "https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/294896846_135708745817122_4487126064901018627_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=6-eTQ94u2foAX9SHROl&tn=NGXuGU5wRJMu9J36&_nc_ht=scontent-ord5-2.xx&oh=00_AfD-PbCghkSs_nZQzy1-XJj_KTWqXpf0VIUg5BhjLcY-Dg&oe=63A2B8A0",
+        image: "https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/294896846_135708745817122_4487126064901018627_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=c_CfmS_rZysAX-MATJz&tn=NGXuGU5wRJMu9J36&_nc_ht=scontent-ord5-2.xx&oh=00_AfAHmF-M6j0t3yoFM9D5Uq8fAjUGpoNZv9JouapT5jPmAQ&oe=63B09060",
         alt: "Help Hounds Band",
         artist: "Help Hounds",
         project: "The Knockoning Single",
